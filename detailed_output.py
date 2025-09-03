5e90f20dbd527a74a9eb74bedc6e6278795e6708
MPADILLA-25 <marissapadilla.hrm@gmail.com>
2025-09-02 22:27:38 -0700

    Add files via upload

---
+++ detailed_output.py
@@ -0,0 +1,5 @@
+def message_to_user():
+    print("User has assigned three numbers (32, 57, 99) to variables num1, num2, and num3. The input entered will be processed to find the largest value. After processing, the largest value found was num3: 99 of the three inputs entered.")
+
+message_to_user()
+
